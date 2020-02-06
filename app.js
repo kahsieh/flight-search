332faf0ac68b1ff3d3a7aa8d7ee69f8127e77805
quentintruong <quentintruong@users.noreply.github.com>
2020-02-05 20:45:59 -0800

    create nodejs backend

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const path = require("path");
+const app = express();
+
+
+app.use(express.static('public'))
+app.use(function(req, res) {
+  res.status(404).sendFile(path.join(__dirname, "/public/404.html"))
+})
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
