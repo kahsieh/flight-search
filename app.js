a141dee687eb33c194336afc3cad0737dfe429e6
quentintruong <quentintruong@users.noreply.github.com>
2020-02-05 22:35:30 -0800

    enable https

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-
 app.use(express.static('public'))
 app.use(function(req, res) {
   res.status(404).sendFile(path.join(__dirname, "/public/404.html"))
