87437d9fa109ce737d6e8af054d5b10aad7462d1
Derek Chu <derekchu53789@gmail.com>
2020-03-05 15:15:57 -0800

    Added autocomplete to profile page

--- public/profile.js
+++ public/profile.js
@@ -13,4 +13,22 @@ All Rights Reserved.
 addEventListener("load", () => {
   // Initialize Materialize selects.
   M.FormSelect.init(qsa("select"), {});
+
+  //Initialize autocompletes
+  const trim = e =>
+      e.value = e.value.includes(" - ") ? e.value.split(" - ")[1] : e.value;
+  let autocomplete_airline =
+    qs("#airline")
+  M.Autocomplete.init(autocomplete_airline, {
+      data: airlines,
+      onAutocomplete: () => trim(autocomplete_airline),
+      limit: 5
+  });
+  let autocomplete_airport =
+    qs("#airport");
+  M.Autocomplete.init(autocomplete_airport, {
+    data: airports,
+    onAutocomplete: () => trim(autocomplete_airport),
+    limit: 5
+  });
 });
