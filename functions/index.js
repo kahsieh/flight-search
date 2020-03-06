64b6cb0c8c2b13aeb31cf58d5a58657307fefa80
leo-liu1 <leoliu1@g.ucla.edu>
2020-03-05 17:18:49 -0800

    initialized cloud functions

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+/*
+Five Peas Flight Search
+app.js
+
+Copyright (c) 2020 Derek Chu, Kevin Hsieh, Leo Liu, Quentin Truong.
+All Rights Reserved.
+*/
+"use strict";
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
